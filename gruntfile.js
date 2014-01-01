c29b954b9d54d309794c6ac3ebee3962825df755
Luke Woodward <luke.woodward@talk21.com>
2014-01-01 13:22:22 +0000

    Reinstate linting to gruntfile.js
    The warning about ascii_only not being camel-cased was fixed by quoting
    the identifier.  Also, the file needed two-space indents replacing with
    four-space indents.

--- gruntfile.js
+++ gruntfile.js
@@ -20,103 +20,104 @@
  */
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: '\n\n',
-        stripBanners: true,
-        banner: grunt.file.read('banner.txt')
-      },
-      dist: {
-        src: ['js/core.js',
-              'js/messages.js',
-              'js/util.js',
-              'js/time.js',
-              'js/competitor.js',
-              'js/age-class.js',
-              'js/age-class-set.js',
-              'js/course.js',
-              'js/event.js',
-              'js/chart-types.js',
-              'js/competitor-selection.js',
-              'js/csv-reader.js',
-              'js/si-reader.js',
-              'js/input.js',
-              'js/competitor-listbox.js',
-              'js/class-selector.js',
-              'js/comparison-selector.js',
-              'js/statistics-selector.js',
-              'js/chart-type-selector.js',
-              'js/chart-popup-data.js',
-              'js/chart-popup.js',
-              'js/chart.js',
-              'js/results-table.js',
-              'js/viewer.js'],
-        dest: '<%= pkg.name %>.js',
-        nonull: true
-      }
-    },    
-    uglify: {
-      options: {
-        preserveComments: "some",
-        beautify: {
-          ascii_only: true
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: '\n\n',
+                stripBanners: true,
+                banner: grunt.file.read('banner.txt')
+            },
+            dist: {
+                src: ['js/core.js',
+                      'js/messages.js',
+                      'js/util.js',
+                      'js/time.js',
+                      'js/competitor.js',
+                      'js/age-class.js',
+                      'js/age-class-set.js',
+                      'js/course.js',
+                      'js/event.js',
+                      'js/chart-types.js',
+                      'js/competitor-selection.js',
+                      'js/csv-reader.js',
+                      'js/si-reader.js',
+                      'js/input.js',
+                      'js/competitor-listbox.js',
+                      'js/class-selector.js',
+                      'js/comparison-selector.js',
+                      'js/statistics-selector.js',
+                      'js/chart-type-selector.js',
+                      'js/chart-popup-data.js',
+                      'js/chart-popup.js',
+                      'js/chart.js',
+                      'js/results-table.js',
+                      'js/viewer.js'
+                ],
+                dest: '<%= pkg.name %>.js',
+                nonull: true
+            }
+        },    
+        uglify: {
+            options: {
+                preserveComments: "some",
+                beautify: {
+                    "ascii_only": true
+                }
+            },
+            dist: {
+                files: {
+                    '<%= pkg.name %>.min.js': ['<%= pkg.name %>.js']
+                }
+            }
+        },
+        qunit: {
+            src: ['qunit-tests.html'],
+            minified: ['qunit-tests-min.html']
+        },
+        jshint: {
+            files: ['gruntfile.js', 'js/*.js', 'test/*-test.js'],
+            options: {
+                // options here to override JSHint defaults
+                plusplus: true,
+                eqeqeq: true,
+                undef: true,
+                unused: true,
+                camelcase: true,
+                curly: true,
+                es3: true,
+                forin: true,
+                immed: true,
+                indent: 4,
+                globals: {
+                    "$": false,
+                    d3: false,
+                    window: false,
+                    document: false,
+                    setTimeout: false,
+                    clearTimeout: false,
+                    alert: false,
+                    SplitsBrowser: true,
+                  
+                    // QUnit globals
+                    QUnit: false,
+                    module: false,
+                    expect: false,
+                  
+                    // Test namespace.
+                    SplitsBrowserTest: true
+                }
+            }
         }
-      },
-      dist: {
-        files: {
-          '<%= pkg.name %>.min.js': ['<%= pkg.name %>.js']
-        }
-      }
-    },
-    qunit: {
-      src: ['qunit-tests.html'],
-      minified: ['qunit-tests-min.html']
-    },
-    jshint: {
-      files: ['js/*.js', 'test/*-test.js'],
-      options: {
-        // options here to override JSHint defaults
-        plusplus: true,
-        eqeqeq: true,
-        undef: true,
-        unused: true,
-        camelcase: true,
-        curly: true,
-        es3: true,
-        forin: true,
-        immed: true,
-        indent: 4,
-        globals: {
-          "$": false,
-          d3: false,
-          window: false,
-          document: false,
-          setTimeout: false,
-          clearTimeout: false,
-          alert: false,
-          SplitsBrowser: true,
-          
-          // QUnit globals
-          QUnit: false,
-          module: false,
-          expect: false,
-          
-          // Test namespace.
-          SplitsBrowserTest: true
-        }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('test', ['jshint', 'qunit:src']);
+    grunt.registerTask('test', ['jshint', 'qunit:src']);
 
-  grunt.registerTask('default', ['jshint', 'qunit:src', 'concat', 'uglify', 'qunit:minified']);
+    grunt.registerTask('default', ['jshint', 'qunit:src', 'concat', 'uglify', 'qunit:minified']);
 
 };
