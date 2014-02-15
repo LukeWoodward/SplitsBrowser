b7a80dff024d85c82e9cd4918b3635eb162a1d26
Luke Woodward <luke.woodward@talk21.com>
2014-02-15 15:24:42 +0000

    Add tests for SI HTML formats to input-test.js
    Serves as a test that SplitsBrowser can correctly detect data formats
    and use the correct parser.

--- test/input-test.js
+++ test/input-test.js
@@ -32,17 +32,48 @@
         var csvData = "Example, 4\r\nJohn,Smith,ABC,10:34,02:57,01:39,03:31,02:01,00:23\r\nFred,Baker,DEF,12:12,02:42,01:51,04:00,01:31,00:30\r\n\r\n" + 
                       "Another example class, 5\r\nJane,Palmer,GHI,11:22,02:50,01:44,03:29,01:40,03:09,00:28\r\nFaye,Claidey,JKL,10:58,02:55,02:00,03:48,01:49,03:32,00:37";
         
-        var result = parseEventData(csvData);
-        assert.ok(result !== null, "There should be an array of classes returned");
-        assert.ok(result instanceof Event, "An event should be returned");
-        assert.strictEqual(result.classes.length, 2, "Two classes should be read in");
-        assert.strictEqual(result.courses.length, 2, "Two courses should be read in");
+        var eventData = parseEventData(csvData);
+        assert.ok(eventData instanceof Event, "An event should be returned");
+        assert.strictEqual(eventData.classes.length, 2, "Two classes should be read in");
+        assert.strictEqual(eventData.courses.length, 2, "Two courses should be read in");
     });
     
     QUnit.test("Can read in 'SI' semicolon-delimited data", function (assert) { 
         var siData = SI_HEADER + "0;1;2;Smith;John;5;6;7;8;11:27:45;10;06:33;12;13;14;ABC;16;17;Test class;19;20;21;22;23;24;25;26;27;28;29;30;31;32;33;34;35;36;37;38;Test course;4.1;140;3;1;44;45;208;01:50;227;03:38;212;06:02";
         var eventData = parseEventData(siData);
-        assert.ok(eventData !== null, "There should be an array of classes returned");
+        assert.ok(eventData instanceof Event, "An event should be returned");
+        assert.strictEqual(eventData.classes.length, 1, "One class should be read in");
+        assert.strictEqual(eventData.courses.length, 1, "One course should be read in");
+    });
+    
+    QUnit.test("Can read in SI HTML preformatted data", function (assert) {
+        var siHtmlData = '<html><head></head><body>\n<pre>\n<font size="2"><b>   Test course 1 (2)</b></font><font size="2"><b>   2.7 km     35 m</b></font>\n' +
+            '<font size="2"><b> </b></font><font size="2"><b> </b></font><font size="2"><b> </b></font><font size="2"><b>   </b></font>   1(138)     2(152)     3(141)    F  \n' +
+            '<font size="2"><b> 1</b></font><font size="2"><b> 165</b></font><font size="2"><b> Test runner</b></font><font size="2"><b>   09:25</b></font>  01:47    04:02    08:13    <font size="2"><b>   09:25</b></font>\n' +
+            '<font size="2"><b>   </b></font><font size="2"><b>   </b></font><font size="2"><b>   TEST</b></font><font size="2"><b>   </b></font>  01:47    02:15    04:11    <font size="2"><b>   01:12</b></font>\n' +
+            '</pre></body></html>';
+        var eventData = parseEventData(siHtmlData);
+        assert.ok(eventData instanceof Event, "An event should be returned");
+        assert.strictEqual(eventData.classes.length, 1, "One class should be read in");
+        assert.strictEqual(eventData.courses.length, 1, "One course should be read in");
+    });
+    
+    QUnit.test("Can read in SI HTML tabular data", function (assert) {
+        var siHtmlData = '<body>\n<div id=reporttop>\n<table width=1105px style="table-layout:auto;">\n<tr><td><nobr>Event title</nobr></td><td id=rb><nobr>Sun 01/02/2013 12:34</nobr></td></tr>\n</table>\n' +
+                         '<hr>\n</div>\n<table id=ln><tr><td>&nbsp</td></tr></table>\n<table width=1105px>\n<tbody>\n' +
+                         '<tr><td id=c12><nobr>   Test course 1 (21)</nobr></td><td id=c12><nobr>   2.7 Km</nobr></td><td id=c12><nobr>   35 m</nobr></td><td id="header" ></td>\n' +
+                         '</tr>\n</tbody>\n</table>\n<table width=1105px>\n<col width=32px>\n<col width=39px>\n<col width=133px>\n<thead>\n' + 
+                         '<tr><th id=rb>Pl</th><th id=rb>Stno</th><th>Name</th><th id=rb>Time</th><th id=rb></th><th id=rb></th></tr>\n' +
+                         '</thead><tbody></tbody></table>\n<table width=1105px>\n<col width=32px>\n<col width=39px>\n<col width=133px>\n<tbody>\n' +
+                         '<tr><td id=c12><nobr>   </nobr></td><td id=c12><nobr>   </nobr></td><td id=c12><nobr>   </nobr></td><td id=c12><nobr>   </nobr></td>' +
+                         '<td id=c12><nobr>   1(138)  </nobr></td><td id=c12><nobr>   2(152)  </nobr></td><td id=c12><nobr>   3(141)  </nobr></td><td id=c12><nobr>   F</nobr></td></tr>\n' +
+                         '<tr><td id=c12><nobr>   1</nobr></td><td id=c12><nobr>   165</nobr></td><td id=c12><nobr>   Test runner</nobr></td><td id=c12><nobr>   09:25</nobr></td>' +
+                         '<td id=c12><nobr>   01:47</nobr></td>  <td id=c12><nobr>   04:02</nobr></td>  <td id=c12><nobr>   08:13</nobr></td>  <td id=c12><nobr>   09:25</nobr></td></tr>\n' +
+                         '<tr><td id=c12><nobr>   </nobr></td><td id=c12><nobr>   </nobr></td><td id=c12><nobr>   TEST</nobr></td><td id=c12><nobr>   </nobr></td>' +
+                         '<td id=c12><nobr>   01:47</nobr></td>  <td id=c12><nobr>   02:15</nobr></td>  <td id=c12><nobr>   04:11</nobr></td>  <td id=c12><nobr>   01:12</nobr></td></tr>\n' +
+                         '<tr><td id=c10><nobr>&nbsp</nobr></td></tr>\n<tr><td id=c10><nobr>&nbsp</nobr></td></tr>\n</tbody>\n</table>\n</body>\n</html>';    
+        var eventData = parseEventData(siHtmlData);
+        assert.ok(eventData instanceof Event, "An event should be returned");
         assert.strictEqual(eventData.classes.length, 1, "One class should be read in");
         assert.strictEqual(eventData.courses.length, 1, "One course should be read in");
     });
