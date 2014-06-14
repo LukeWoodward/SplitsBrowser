e416f0c6e4d5ccf3e69b1aebd00c18509c8b6dae
Luke Woodward <luke.woodward@talk21.com>
2014-06-14 18:52:17 +0100

    Update French Belgian translation

--- lang/messages-fr_be.js
+++ lang/messages-fr_be.js
@@ -21,14 +21,14 @@
 SplitsBrowser.Messages = {
 
     ApplicationVersion: "SplitsBrowser - Version $$VERSION$$",
-    Language: "FranÃ§ais",
+    Language: "Français",
     
     MispunchedShort: "pm",
     NonCompetitiveShort: "hc",
     
-    StartName: "DÃ©part",
-    ControlName: "ContrÃ´le $$CODE$$",
-    FinishName: "ArrivÃ©e",
+    StartName: "Départ",
+    ControlName: "Contrôle $$CODE$$",
+    FinishName: "Arrivée",
 
     // The start and finish, as they appear at the top of the chart.
     StartNameShort: "D",
@@ -37,17 +37,17 @@ SplitsBrowser.Messages = {
     // Button labels.
     SelectAllCompetitors: "Tous",
     SelectNoCompetitors: "Aucun",
-    SelectCrossingRunners: "Coureurs rencontrÃ©s",
+    SelectCrossingRunners: "Coureurs rencontrés",
     
     LowerXAxisChartLabel: "Temps (min)",
 
     // Chart type names and Y-axis labels.
     SplitsGraphChartType: "Graphique",
     SplitsGraphYAxisLabel: "Temps (min)",
-    RaceGraphChartType: "Heures rÃ©elles",
+    RaceGraphChartType: "Heures réelles",
     RaceGraphYAxisLabel: "Temps",
-    PositionAfterLegChartType: "Place aprÃ¨s tronÃ§on",
-    SplitPositionChartType: "Place intermÃ©diaire",
+    PositionAfterLegChartType: "Place après tronçon",
+    SplitPositionChartType: "Place intermédiaire",
     PositionYAxisLabel: "Place", // Shared between position-after-leg and split-position.
     PercentBehindChartType: "% Retard",
     PercentBehindYAxisLabel: "% Retard",
@@ -57,7 +57,7 @@ SplitsBrowser.Messages = {
     
     ClassSelectorLabel: "Circuit: ",
     AdditionalClassSelectorLabel: "et",
-    NoClassesLoadedPlaceholder: "[Aucun circuit chargÃ©]",
+    NoClassesLoadedPlaceholder: "[Aucun circuit chargé]",
     
     // Placeholder text shown when additional classes are available to be
     // selected but none have been selected.
@@ -71,12 +71,12 @@ SplitsBrowser.Messages = {
     CompareWithAnyRunnerLabel: "Coureur: ",
     // Warning message shown to the user when a comparison option cannot be
     // chosen because the course has no winner.
-    CannotCompareAsNoWinner: "Impossible de comparer avec '$$OPTION$$' car aucun coureur de ce circuit n'a terminÃ© le parcours.",
+    CannotCompareAsNoWinner: "Impossible de comparer avec '$$OPTION$$' car aucun coureur de ce circuit n'a terminé le parcours.",
     
     // Label of checkbox that shows the original data as opposed to the
     // 'repaired' data.  This only appears if data that needs repair has been
     // loaded.
-    ShowOriginalData: "Show original data",
+    ShowOriginalData: "Afficher les données d'origine",
   
     // Tooltip of 'Show original' checkbox.  This appears when SplitsBrowser
     // deduces that some of the cumulatives times in the data shown are
@@ -85,12 +85,12 @@ SplitsBrowser.Messages = {
                              "Utilisez la case à cocher pour afficher les données originales ou modifiées.",
     
     StatisticsTotalTime: "Temps total",
-    StatisticsSplitTime: "Temps intermÃ©diaire",
+    StatisticsSplitTime: "Temps intermédiaire",
     StatisticsBehindFastest: "Retard # 1",
     StatisticsTimeLoss: "Temps perdu",
     
-    ResultsTableHeaderSingleControl: "1 contrÃ´le",
-    ResultsTableHeaderMultipleControls: "$$NUM$$ contrÃ´les",
+    ResultsTableHeaderSingleControl: "1 contrôle",
+    ResultsTableHeaderMultipleControls: "$$NUM$$ contrôles",
     ResultsTableHeaderCourseLength: "$$DISTANCE$$km",
     ResultsTableHeaderClimb: "$$CLIMB$$m",
     
@@ -100,7 +100,7 @@ SplitsBrowser.Messages = {
     
     // Alert message shown when you click 'Crossing runners' but there are no
     // crossing runners to show.
-    RaceGraphNoCrossingRunners: "$$NAME$$ n'a rencontrÃ© aucun coureur.",
+    RaceGraphNoCrossingRunners: "$$NAME$$ n'a rencontré aucun coureur.",
     RaceGraphDisabledAsStartTimesMissing: "Ce graphique ne peut être affiché car il n'y a pas d'heures de départ dans les données.",
     
     LoadFailedHeader: "SplitsBrowser \u2013 Erreur",
@@ -111,15 +111,15 @@ SplitsBrowser.Messages = {
     
     // Chart popups.
     
-    SelectedClassesPopupHeader: "Circuits sÃ©lectionnÃ©s",
+    SelectedClassesPopupHeader: "Circuits sélectionnés",
     
     // Placeholder text shown when the Selected classes dialog is empty,
     // because no competitors registered a split for the control, or those
     // that did only registered a dubious split.
-    SelectedClassesPopupPlaceholder: "Aucun coureur n'a terminÃ© ce circuit",
+    SelectedClassesPopupPlaceholder: "Aucun coureur n'a terminé ce circuit",
     
     // Header for the 'Fastest leg time' popup dialog.
-    FastestLegTimePopupHeader: "IntermÃ©diaire le plus rapide $$START$$ - $$END$$",
+    FastestLegTimePopupHeader: "Intermédiaire le plus rapide $$START$$ - $$END$$",
 
     // Header for the nearby-competitors dialog on the race graph.
     NearbyCompetitorsPopupHeader: "$$START$$ - $$END$$: $$CONTROL$$",
@@ -128,10 +128,10 @@ SplitsBrowser.Messages = {
     // graph when there aren't any competitors visiting the control within the
     // +/- 2 minute window.
     NoNearbyCompetitors: "Aucun coureur",
-    
+
     // Link that appears at the top and opens SplitsBrowser with the settings
     // (selected classes, competitors, comparison, chart type, etc.) that are
     // currently shown.
-    DirectLink: "Link",
-    DirectLinkToolTip: "Links to a URL that opens SplitsBrowser with the current settings"
+    DirectLink: "Lien",
+    DirectLinkToolTip: "Lien vers une URL qui lance SplitsBrowser avec les paramètres en cours"	
 };
