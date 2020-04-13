18eda42350cb77aef172198760927c006539aa20
Luke Woodward <luke.woodward@talk21.com>
2020-04-13 09:57:47 +0100

    Add team object and basic test for it.

---
+++ test/team-test.js
@@ -0,0 +1,33 @@
+/*
+ *  SplitsBrowser - Team tests.
+ *
+ *  Copyright (C) 2000-2020 Dave Ryder, Reinhard Balling, Andris Strazdins,
+ *                          Ed Nash, Luke Woodward
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+(function (){
+    "use strict";
+
+    var Team = SplitsBrowser.Model.Team;
+
+    QUnit.module("Team");
+    
+    QUnit.test("Can create a team", function (assert) {
+        var team = new Team("Team 1", "ABC");
+        assert.strictEqual(team.name, "Team 1");
+        assert.strictEqual(team.club, "ABC");
+    });
+})();
