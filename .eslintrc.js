daf30d85384a2d1d0e861e3a84658a3f0a7e996f
Luke Woodward <luke.woodward@talk21.com>
2020-07-04 15:02:04 +0100

    Adjust ESLint configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es6": true
+        "es6": true,
+        "jquery": true,
+        "qunit": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
@@ -10,9 +12,7 @@ module.exports = {
     "globals": {
         "SplitsBrowser": "writable",
         "SplitsBrowserTest": "writable",
-        "d3": "readonly",
-        "$": "readonly",
-        "QUnit": "readonly"
+        "d3": "readonly"
     },
     "rules": {
         "indent": [
